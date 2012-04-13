abd13d98b1da2af5640d68a43eb4bc3afb402c7b
zeke <zeke@sikelianos.com>
2012-04-13 15:35:11 -0700

    Added support for unpushed repos, and added a README.

--- git_audit.rb
+++ git_audit.rb
@@ -1,23 +1,29 @@
-# This script traverses the given directory in search of repos with unstaged or unpushed local changes.
+# This script finds git repos with unstaged changes and/or unpushed commits
+# (in the currently checked-out branch).
+
+require 'ostruct'
 
 abort "Please specify a base directory, e.g. `ruby git_audit.rb ~/projects" unless ARGV.first
 
 basedir = ARGV.first.sub(/\/$/, '')
 
-repos = %x[find #{basedir} -name '*.git' -type d | sort].split("\n")
-
-repos.each do |repo|
-  repo = repo.sub(/\.git$/, '')
-  
-  status = %x[cd #{repo}; git status]
-  # puts status
-
-  if status =~ /working directory clean/
-    
-  else
-    puts repo
-    # puts status
-    # puts "\n\n"
-  end
-
+repos = %x[find #{basedir} -name '*.git' -type d | sort].split("\n").map do |path|
+  repo = OpenStruct.new
+  repo.path = path.sub(/\.git$/, '')
+  repo.status = %x[cd #{repo.path}; git status]
+  repo
 end
+
+clean = repos.select {|_| _.status =~ /working directory clean/ }
+unpushed = repos.select {|_| _.status =~ /branch is ahead/ }
+unstaged = repos - clean - unpushed
+
+puts "\nUNSTAGED"
+puts unstaged.map{|_| _.path }.join("\n")
+
+puts "\nUNPUSHED"
+puts unpushed.map{|_| _.path }.join("\n")
+
+puts "\nCLEAN"
+puts clean.map{|_| _.path }.join("\n")
+
